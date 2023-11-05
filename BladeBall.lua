4a9cd12c9d998e1eb68ce279341cc195c8cb4bdd
Imposterundetalelover <112672496+Imposterundetalelover@users.noreply.github.com>
2023-11-05 11:24:43 +0800

    Update BladeBall.lua

--- BladeBall.lua
+++ BladeBall.lua
@@ -2,9 +2,9 @@
 
 local Library = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
 local SystemWindow = Library:CreateWindow({
-    Name = "TroXer Hub | Who Exsist",
-    LoadingTitle = "TroXer Hub is Loading..",
-    LoadingSubtitle = "By ArteeSo",
+    Name = "clownware Hub | Who Exsist",
+    LoadingTitle = "clownware Hub is Loading..",
+    LoadingSubtitle = "By genl",
     ConfigurationSaving = {
        Enabled = true,
        FolderName = nil, -- Create a custom folder for your hub/game
@@ -12,18 +12,18 @@ local SystemWindow = Library:CreateWindow({
     },
     Discord = {
        Enabled = true,
-       Invite = "GajSbcfHxh", -- The Discord invite code, do not include discord.gg/. E.g. discord.gg/ABCD would be ABCD
+       Invite = "JHnd2TGy", -- The Discord invite code, do not include discord.gg/. E.g. discord.gg/ABCD would be ABCD
        RememberJoins = false -- Set this to false to make them join the discord every time they load it up
     },
     KeySystem = true, -- Set this to true to use our key system
     KeySettings = {
-       Title = "TroXer Protection",
+       Title = "clownware protection",
        Subtitle = "Key System",
-       Note = "Join to discord {https://discord.gg/GajSbcfHxh}",
+       Note = "Join to discord {https://discord.gg/JHnd2TGy}",
        FileName = "TroXerKey", -- It is recommended to use something unique as other scripts using Rayfield may overwrite your key file
        SaveKey = true, -- The user's key will be saved, but if you change the key, they will be unable to use your script
        GrabKeyFromSite = false, -- If this is true, set Key below to the RAW site you would like Rayfield to get the key from
-       Key = {"Its A Key"} -- List of keys that will be accepted by the system, can be RAW file links (pastebin, github etc) or simple strings ("hello","key22")
+       Key = {"Privatekeyhubidkjoestoplookinfdombshitihateyouiwilleatcrocsikliekormfdsfdsfsdfadsaddchingchongsingsong"} -- List of keys that will be accepted by the system, can be RAW file links (pastebin, github etc) or simple strings ("hello","key22")
     }
 })
 
